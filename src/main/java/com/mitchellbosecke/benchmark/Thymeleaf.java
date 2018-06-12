e89c45679473119b992b21e25757113b19867c7c
linux_china <linux_china@hotmail.com>
2018-06-12 13:20:45 -0700

    polishing

--- src/main/java/com/mitchellbosecke/benchmark/Thymeleaf.java
+++ src/main/java/com/mitchellbosecke/benchmark/Thymeleaf.java
@@ -1,18 +1,19 @@
 package com.mitchellbosecke.benchmark;
 
+import freemarker.template.TemplateException;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Setup;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.TemplateSpec;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.context.IContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
 import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.util.Locale;
-
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.Setup;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.Context;
-import org.thymeleaf.context.IContext;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
-
-import freemarker.template.TemplateException;
 
 public class Thymeleaf extends BaseBenchmark {
 
@@ -20,17 +21,20 @@ public class Thymeleaf extends BaseBenchmark {
 
     private IContext context;
 
+    TemplateSpec templateSpec;
+
     @Setup
     public void setup() throws IOException {
         engine = new TemplateEngine();
         engine.setTemplateResolver(new ClassLoaderTemplateResolver());
         context = new Context(Locale.getDefault(), getContext());
+        templateSpec = new TemplateSpec("templates/stocks.thymeleaf.html", TemplateMode.HTML);
     }
 
     @Benchmark
     public String benchmark() throws TemplateException, IOException {
         Writer writer = new StringWriter();
-        engine.process("templates/stocks.thymeleaf.html", context, writer);
+        engine.process(templateSpec, context, writer);
         return writer.toString();
     }
 
